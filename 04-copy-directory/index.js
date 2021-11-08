0bb8be4c4ed6153b7b9d7160b371b09c07189bf9
Yevhenii <sharovskiyevgeniy@gmail.com>
2021-11-09 00:13:32 +0200

    feat: complete 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
 
+const fs = require('fs/promises');
+const path = require('path');
+
+const link = path.join(__dirname, 'files')
+
+
+// fs.stat(link+'-copy',function(err){
+//     if(err){
+//         fs.rm(link+'-copy',{recursive:true,force:true})
+//         console.log('lol');
+//     }
+//     console.log('kek');
+//     fs.mkdir(link+'-copy',{recursive:true})
+//     copyFile()
+// })
+
+
+
+async function copyFile() {
+    await fs.rm(link + '-copy', { recursive: true, force: true })
+    fs.mkdir(link + '-copy', { recursive: true })
+    const files = await fs.readdir(link, { withFileTypes: true });
+    for (const file of files){
+        fs.copyFile(path.join(link, file.name), link + '-copy\\' + file.name)
+    }
+}
+
+copyFile()
